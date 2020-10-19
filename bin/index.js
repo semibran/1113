d6ad78818ef7a69010bff762efb9db1e963b46ac
Brandon Semilla <semibran@gmail.com>
2020-10-19 01:16:23 -0700

    excess first pass

--- bin/index.js
+++ bin/index.js
@@ -1,14 +1,15 @@
 const funcs = {
-	anytodec: require("../lib/any-to-dec"),
-	dectoany: require("../lib/dec-to-any"),
-	anytoany: require("../lib/any-to-any"),
-	signmag: require("../lib/bin-signmag"),
+	"any-to-dec": require("../lib/any-to-dec"),
+	"dec-to-any": require("../lib/dec-to-any"),
+	"any-to-any": require("../lib/any-to-any"),
+	"signmag": require("../lib/bin-signmag"),
 	"1c": require("../lib/bin-1c"),
 	"2c": require("../lib/bin-2c"),
+	"xs": require("../lib/bin-xs"),
 }
 
 let input = process.argv.slice(2)
 let convert = funcs[input[0]]
 let args = input.slice(1)
 let output = convert(...args)
-console.log(input, "->", output)
+console.log(output)
