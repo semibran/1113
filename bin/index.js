56f6b9ee39c2add0ad7defbfc0071d9faa7ecdbd
Brandon Semilla <semibran@gmail.com>
2020-10-19 01:22:58 -0700

    refuse nonexistent commands

--- bin/index.js
+++ bin/index.js
@@ -10,7 +10,13 @@ const funcs = {
 }
 
 let input = process.argv.slice(2)
-let convert = funcs[input[0]]
+let funcid = input[0]
+let convert = funcs[funcid]
+if (!convert) {
+	console.log("no function found for " + funcid)
+	process.exit()
+}
+
 let args = input.slice(1)
 let output = convert(...args)
 console.log(output)
