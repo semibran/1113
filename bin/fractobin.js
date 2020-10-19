e28910b45206611b43a422a67c15d6d391490094
Brandon Semilla <semibran@gmail.com>
2020-10-18 23:44:12 -0700

    add base converters

--- bin/fractobin.js
+++
@@ -1,9 +0,0 @@
-const fractobin = require("./fractobin")
-
-let dec = process.argv[2]
-if (!dec) {
-	console.log("Provide a decimal fraction as an argument.")
-} else {
-	let bin = fractobin(eval(dec))
-	console.log(bin)
-}
--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,19 @@
-const todec = require("../lib/todec")
-const funcs = { todec }
+const funcs = {
+	anytodec: require("../lib/anytodec"),
+	dectoany: require("../lib/dectoany"),
+	anytoany: require("../lib/anytoany"),
+	fractobin: require("../lib/fractobin")
+}
+
+// number : {
+// 	base : int
+// 	int : str
+// 	frac : str
+// 	repeat : bool
+// }
 
 let input = process.argv.slice(2)
 let convert = funcs[input[0]]
-let output = convert(...input.slice(1))
+let args = input.slice(1)
+let output = convert(...args)
 console.log(input, "->", output)
