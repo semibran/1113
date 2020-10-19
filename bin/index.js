14beb92622357153e70c054a73655f9c6425f942
Brandon Semilla <semibran@gmail.com>
2020-10-19 03:22:52 -0700

    rename modules

--- bin/index.js
+++ bin/index.js
@@ -1,16 +1,16 @@
 const funcs = {
 	"any-to-dec": require("../lib/any-to-dec"),
-	"dec-to-any": require("../lib/dec-to-any"),
-	"any-to-any": require("../lib/any-to-any"),
+	"dec-to-any": require("../lib/float-from-dec"),
+	"any-to-any": require("../lib/float-from-any"),
 	"2c-to-dec": require("../lib/2c-to-dec"),
 	"bias-to-dec": require("../lib/bias-to-dec"),
-	"signmag": require("../lib/bin-signmag"),
-	"1c": require("../lib/bin-1c"),
-	"2c": require("../lib/bin-2c"),
-	"xs": require("../lib/bin-xs"),
-	"bias": require("../lib/bin-bias"),
-	"add2c": require("../lib/bin-add2c"),
-	"bcit": require("../lib/dec-to-bcit"),
+	"signmag": require("../lib/signmag-from-dec"),
+	"1c": require("../lib/1c-from-dec"),
+	"2c": require("../lib/2c-from-dec"),
+	"xs": require("../lib/xc-from-dec"),
+	"bias": require("../lib/bias-from-dec"),
+	"add2c": require("../lib/2c-add"),
+	"bcit": require("../lib/bcit-from-dec"),
 }
 
 let input = process.argv.slice(2)
